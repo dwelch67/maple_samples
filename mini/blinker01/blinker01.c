1ae3c81bfbfbf1cdfc531c9953ef5567a900ac8c
David Welch <dwelch@dwelch.com>
2012-06-11 00:12:43 -0400

    adding blinker01 for the mini

---
+++ mini/blinker01/blinker01.c
@@ -0,0 +1,48 @@
+//http://gitorious.org/~tormod/unofficial-clones/dfuse-dfu-util
+//dfu-util -c 1 -i 0 -a 1 -D blinker01.gcc.thumb.bin
+
+void PUT32 ( unsigned int, unsigned int );
+unsigned int GET32 ( unsigned int );
+void dummy ( unsigned int );
+
+#define GPIOB_BASE 0x40010C00
+#define GPIOB_CRL  (GPIOB_BASE+0x00)
+#define GPIOB_CRH  (GPIOB_BASE+0x04)
+#define GPIOB_IDR  (GPIOB_BASE+0x08)
+#define GPIOB_ODR  (GPIOB_BASE+0x0C)
+#define GPIOB_BSRR (GPIOB_BASE+0x10)
+#define GPIOB_BRR  (GPIOB_BASE+0x14)
+#define GPIOB_LCKR (GPIOB_BASE+0x18)
+
+int notmain ( void )
+{
+    unsigned int ra;
+
+    //pb1 output
+    ra=GET32(GPIOB_CRL);
+    ra&=~(0xF0);
+    //mode 01  cnf 00
+    //0001
+    ra|=0x10;
+    PUT32(GPIOB_CRL,ra);
+
+    while(1)
+    {
+        PUT32(GPIOB_BSRR,0x00000002);
+        for(ra=0;ra<100000;ra++) dummy(ra);
+        PUT32(GPIOB_BSRR,0x00020000);
+        for(ra=0;ra<100000;ra++) dummy(ra);
+        PUT32(GPIOB_BSRR,0x00000002);
+        for(ra=0;ra<100000;ra++) dummy(ra);
+        PUT32(GPIOB_BSRR,0x00020000);
+        for(ra=0;ra<100000;ra++) dummy(ra);
+        PUT32(GPIOB_BSRR,0x00000002);
+        for(ra=0;ra<100000;ra++) dummy(ra);
+        PUT32(GPIOB_BSRR,0x00020000);
+        for(ra=0;ra<400000;ra++) dummy(ra);
+    }
+
+
+
+    return(0);
+}
