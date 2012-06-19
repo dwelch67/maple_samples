89c976fc52d3f170b22b89c0dddb85b25bbb58b6
David Welch <dwelch@dwelch.com>
2012-06-19 00:28:55 -0400

    dumb luck, the bootloader enabled GPIOB for the led, we should have done that adding blinker02 example using the system timer and uart01, inits the uart and sends and receives characters

--- mini/blinker02/blinker02.c
+++ mini/blinker02/blinker02.c
@@ -1,13 +1,13 @@
 //http://gitorious.org/~tormod/unofficial-clones/dfuse-dfu-util
-//dfu-util -c 1 -i 0 -a 1 -D blinker01.gcc.thumb.bin
+//dfu-util -c 1 -i 0 -a 1 -D blinker02.gcc.thumb.bin
 
 void PUT32 ( unsigned int, unsigned int );
 unsigned int GET32 ( unsigned int );
+unsigned int GET16 ( unsigned int );
 void dummy ( unsigned int );
 
 #define RCC_BASE 0x40021000
 #define RCC_APB2ENR (RCC_BASE+0x18)
-
 
 #define GPIOB_BASE 0x40010C00
 #define GPIOB_CRL  (GPIOB_BASE+0x00)
@@ -18,6 +18,10 @@ void dummy ( unsigned int );
 #define GPIOB_BRR  (GPIOB_BASE+0x14)
 #define GPIOB_LCKR (GPIOB_BASE+0x18)
 
+#define STK_CTRL (0xE000E010+0x00)
+#define STK_LOAD (0xE000E010+0x04)
+#define STK_VAL  (0xE000E010+0x08)
+
 int notmain ( void )
 {
     unsigned int ra;
@@ -25,6 +29,7 @@ int notmain ( void )
     ra=GET32(RCC_APB2ENR);
     ra|=1<<3;   //GPIOB
     PUT32(RCC_APB2ENR,ra);
+
 
     //pb1 output
     ra=GET32(GPIOB_CRL);
@@ -34,23 +39,22 @@ int notmain ( void )
     ra|=0x10;
     PUT32(GPIOB_CRL,ra);
 
+    PUT32(STK_CTRL,0);
+    PUT32(STK_LOAD,0x00FFFFFF);
+    PUT32(STK_CTRL,1);
+
     while(1)
     {
         PUT32(GPIOB_BSRR,0x00000002);
-        for(ra=0;ra<100000;ra++) dummy(ra);
+        while(1)
+        {
+            if((GET32(STK_VAL)&0x800000)==0x800000) break;
+        }
         PUT32(GPIOB_BSRR,0x00020000);
-        for(ra=0;ra<100000;ra++) dummy(ra);
-        PUT32(GPIOB_BSRR,0x00000002);
-        for(ra=0;ra<100000;ra++) dummy(ra);
-        PUT32(GPIOB_BSRR,0x00020000);
-        for(ra=0;ra<100000;ra++) dummy(ra);
-        PUT32(GPIOB_BSRR,0x00000002);
-        for(ra=0;ra<100000;ra++) dummy(ra);
-        PUT32(GPIOB_BSRR,0x00020000);
-        for(ra=0;ra<400000;ra++) dummy(ra);
+        while(1)
+        {
+            if((GET32(STK_VAL)&0x800000)==0x000000) break;
+        }
     }
-
-
-
     return(0);
 }
